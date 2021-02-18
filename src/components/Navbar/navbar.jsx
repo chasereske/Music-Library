a73d7bdfe904e35637d25dd64e290dee007051cf
chasereske <chasereske@gmail.com>
2021-02-18 15:17:55 -0500

    Added code for the navbar and connected it to the app.jsx file.

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,1 +1,15 @@
 import React from 'react'; 
+
+function Navbar(props){
+    return (
+        <div className="row row-spacer">
+            <div className="col-md-12" style={{padding:0}}>
+                <div>
+                    <h1>Reske's Music Library</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,18 @@
 import axios from 'axios';
 import React, {Component} from 'react'; 
 import AxiosCall from './MusicTable/musicTable';
+import Navbar from './Navbar/navbar';
 
 class App extends Component {
     state = { }
     render() {
         return(
-            <div>
-            <h1>Our React App Using a Component</h1>
-            <AxiosCall/>
+            <div className="container-fluid">
+                <Navbar/>
+                <div className="row row-spacer">
+                    <h1>Our React App Using a Component</h1>
+                    <AxiosCall/>
+                </div>
             </div>
         );
     }
