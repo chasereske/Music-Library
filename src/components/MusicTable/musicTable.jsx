b2fc191838aad261a7ef8afc2bf311ee7f529477
chasereske <chasereske@gmail.com>
2021-02-17 21:09:15 -0500

    Connected the different components via import/exports. Created the axios call. I need to now figure out how to show the axios call and then begin structuring the music table.

--- src/components/MusicTable/musicTable.jsx
+++ src/components/MusicTable/musicTable.jsx
@@ -1,1 +1,14 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
+import axios from 'axios';
+
+
+    function AxiosCall(){
+        return(
+        axios.get('http://www.devcodecampmusiclibrary.com/').then((data) => {
+        console.log(data);
+    }));
+}
+
+
+
+export default AxiosCall;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,1 +1,17 @@
-import React from 'react'; 
+import axios from 'axios';
+import React, {Component} from 'react'; 
+import AxiosCall from './MusicTable/musicTable';
+
+class App extends Component {
+    state = { }
+    render() {
+        return(
+            <div>
+            <h1>Our React App Using a Component</h1>
+            <AxiosCall/>
+            </div>
+        );
+    }
+}
+
+export default App;
