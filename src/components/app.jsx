40c6e92433b5855230dc1b553a2e7acdac548696
chasereske <chasereske@gmail.com>
2021-02-22 15:49:59 -0500

    Added a search bar to the site.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, {Component} from 'react'; 
 import MusicList from './MusicTable/musicTable';
 import Navbar from './Navbar/navbar';
+import SearchBar from './SearchBar/searchBar';
 
 class App extends Component {
     state = { }
@@ -9,6 +10,7 @@ class App extends Component {
         return(
             <div className="container-fluid">
                 <Navbar/>
+                <SearchBar/>
                 <div className="row row-spacer">
                     <h1>Our React App Using a Component</h1>
                     <MusicList/>
